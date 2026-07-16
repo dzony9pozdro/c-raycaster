7a1cf3c1ac2416d463b7efbaf04aceec77468279
Jan Tomaka <jan@tomaka.it>
2026-07-16 19:16:52 +0200

    getting started on ray casting, basic spaghetti working version

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <SDL2/SDL.h>
-#include <stdbool.h>
 #include <math.h>
+#include <stdatomic.h>
+#include <stdbool.h>
 
-#define MAP_W 16
-#define MAP_H 9
-#define CELL 64
+#define MAP_W 12 / 3
+#define MAP_H 9 / 3
+#define CELL 200
 
 typedef struct {
   double x, y;
 } Vec2;
-typedef struct {
 
+typedef struct {
   Vec2 pos;
   Vec2 dir;
   Vec2 vel;
@@ -18,78 +19,149 @@ typedef struct {
 } Camera;
 
 Camera camera_default(void) {
-  Camera c = {.pos = {.x = 0, .y = 0},
+  Camera c = {.pos = {.x = 300, .y = 300},
               .dir = {.x = 1, .y = 0},
               .vel = {.x = 0, .y = 0},
               .deg = 0};
   return c;
 }
 
-void draw_map(SDL_Renderer *renderer) {
-
+void draw_map(SDL_Renderer *renderer, int *debug) {
   int map[MAP_H][MAP_W] = {
-      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-      {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
-      {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
-      {1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1},
-      {1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1},
-      {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-      {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
-      {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
-      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+      {1, 1, 1, 1},
+      {1, 0, 0, 0},
+      {1, 0, 1, 0},
   };
   int px = 0, py = 0;
 
   for (int w = 0; w < MAP_W; w++) {
     for (int h = 0; h < MAP_H; h++) {
-
       px = CELL * w;
       py = CELL * h;
-      if (map[h][w] == 1) {
+      if (map[h][w] == 1 && *debug == 0) {
         SDL_Rect r = {px, py, CELL, CELL};
+        SDL_RenderFillRect(renderer, &r);
+      } else {
+        SDL_Rect r = {0, py, 30000, 1};
         SDL_RenderFillRect(renderer, &r);
       }
     }
+
+    SDL_Rect r = {px, 0, 1, 300000};
+    SDL_RenderFillRect(renderer, &r);
   }
 }
-void draw_player(SDL_Renderer *renderer, Camera *cam) {
 
+void cast_ray(SDL_Renderer *renderer, Camera *cam) {
+  // adjacent
+  double pos_x_bound_rel_x = CELL - fmod(cam->pos.x, CELL);
+  double pos_y_bound_rel_y = CELL - fmod(cam->pos.y, CELL);
+
+  double neg_x_bound_rel_x = -fmod(cam->pos.x, CELL);
+  double neg_y_bound_rel_y = -fmod(cam->pos.y, CELL);
+
+  double y_from_x, x_from_y;
+
+  if (cam->dir.x == 0) {
+    y_from_x = pos_y_bound_rel_y;
+  } else {
+    y_from_x = pos_x_bound_rel_x * (cam->dir.y / cam->dir.x);
+  }
+  if (cam->dir.y == 0) {
+    x_from_y = pos_x_bound_rel_x;
+  } else {
+    x_from_y = pos_y_bound_rel_y * (cam->dir.x / cam->dir.y);
+  }
+
+  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+  // vertical
+  //  first pos x vertical intersection vertices
+  Vec2 posxv1 = {cam->pos.x + pos_x_bound_rel_x, cam->pos.y + y_from_x};
+  SDL_Rect pos_x_fc = {posxv1.x, posxv1.y, 10, 10};
+
+  // first neg x vertical intersection vertices
+  Vec2 negxv1 = {cam->pos.x + neg_x_bound_rel_x, cam->pos.y - y_from_x};
+  SDL_Rect neg_x_fc = {negxv1.x, negxv1.y, 10, 10};
+
+  SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+  SDL_RenderFillRect(renderer, &pos_x_fc);
+  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+  SDL_RenderFillRect(renderer, &neg_x_fc);
+
+  // horizontal
+
+  //  first pos y vertical intersection vertices
+  Vec2 posyv1 = {cam->pos.x + x_from_y, cam->pos.y + pos_y_bound_rel_y};
+  SDL_Rect pos_y_fc = {posyv1.x, posyv1.y, 10, 10};
+
+  //  first neg y vertical intersection vertices
+  Vec2 negyv1 = {cam->pos.x - x_from_y, cam->pos.y + neg_y_bound_rel_y};
+  SDL_Rect neg_y_fc = {negyv1.x, negyv1.y, 10, 10};
+
+  SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+  SDL_RenderFillRect(renderer, &pos_y_fc);
+  SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+  SDL_RenderFillRect(renderer, &neg_y_fc);
+  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+  Vec2 check;
+  Vec2 interval;
+  Vec2 next_check = {.x = check.x + interval.x, .y = check.y + interval.y};
+
+  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+}
+
+void draw_player(SDL_Renderer *renderer, Camera *cam) {
   cam->pos.x += cam->vel.x;
   cam->pos.y += cam->vel.y;
 
-  double line_length = 50; // sqrt(cam->dir.x * cam->dir.y) *
+  double line_length = 280;
 
   cam->vel.x /= 2;
   cam->vel.y /= 2;
 
-  fprintf(stderr, "pos: %.2f, %.2f deg: %.2f dir: %.2f, %.2f \n", cam->pos.x,
-          cam->pos.y, cam->deg, cam->dir.x, cam->dir.y);
-  SDL_Rect p = {cam->pos.x - CELL / 4.0, cam->pos.y - CELL / 4.0, CELL / 2,
-                CELL / 2};
-  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+  SDL_Rect p = {cam->pos.x - CELL / 8.0, cam->pos.y - CELL / 8.0, CELL / 4, CELL / 4};
+
+  SDL_SetRenderDrawColor(renderer, 100, 100, 100, 190);
+
   SDL_RenderFillRect(renderer, &p);
 
+  SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+
+  // sin
+  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+  SDL_RenderDrawLine(renderer, cam->pos.x, cam->pos.y, cam->pos.x,
+                     cam->pos.y + cam->dir.y * line_length);
+  // cos
+
+  SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+  SDL_RenderDrawLine(renderer, cam->pos.x, cam->pos.y,
+                     cam->pos.x + cam->dir.x * line_length, cam->pos.y);
+
+  // dir vector
+  SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
   SDL_RenderDrawLine(renderer, cam->pos.x, cam->pos.y,
                      cam->pos.x + cam->dir.x * line_length,
                      cam->pos.y + cam->dir.y * line_length);
 }
 
 void turn(double direction, Camera *cam) {
-    cam->deg += direction;                // accumulate total angle
-    cam->dir.x = cos(cam->deg);           // rebuild dir FROM the total
-    cam->dir.y = sin(cam->deg);
+  cam->deg += direction;
+  cam->dir.x = cos(cam->deg);
+  cam->dir.y = sin(cam->deg);
 }
-
 
 int main(int argc, char *argv[]) {
   (void)argc;
   (void)argv;
+  int debug = 1;
 
   SDL_Init(SDL_INIT_VIDEO);
 
   SDL_Window *window =
-      SDL_CreateWindow("raycaster", SDL_WINDOWPOS_CENTERED,
-                       SDL_WINDOWPOS_CENTERED, 16 * CELL, 9 * CELL, 0);
+      SDL_CreateWindow("raycaster", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                       MAP_W * CELL, MAP_H * CELL, 0);
 
   SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 
@@ -102,8 +174,7 @@ int main(int argc, char *argv[]) {
   while (running) {
     SDL_Event e;
     while (SDL_PollEvent(&e)) {
-      if (e.type == SDL_QUIT)
-        running = false;
+      if (e.type == SDL_QUIT) running = false;
     }
 
     if (keys[SDL_SCANCODE_W] && cam.vel.y < 300) {
@@ -120,23 +191,22 @@ int main(int argc, char *argv[]) {
     }
 
     if (keys[SDL_SCANCODE_J]) {
-      turn(-0.3, &cam);
+      turn(-0.08, &cam);
     }
 
     if (keys[SDL_SCANCODE_K]) {
-      turn(0.3, &cam);
+      turn(0.08, &cam);
     }
 
-    SDL_SetRenderDrawColor(renderer, 30, 60, 120, 255); // blue-ish
+    SDL_SetRenderDrawColor(renderer, 30, 60, 120, 255);  // blue-ish
     SDL_RenderClear(renderer);
 
     SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
-
-    draw_map(renderer);
+    draw_map(renderer, &debug);
     draw_player(renderer, &cam);
-
+    cast_ray(renderer, &cam);
     SDL_RenderPresent(renderer);
-    SDL_Delay(16);   // ~16ms per frame ≈ 60fps
+    SDL_Delay(16);  // ~16ms per frame ≈ 60fps
   }
 
   SDL_DestroyRenderer(renderer);
