e760c52f2f75f90b1a724f4eec9607f0f70e129e
Jan Tomaka <jan@tomaka.it>
2026-07-19 02:05:57 +0200

    find map wall/filled blocks' 4 vertices, for collision detection later - HACK HACK HACK

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <SDL2/SDL.h>
 #include <math.h>
 #include <stdbool.h>
+
 #define FOV 100
-#define MAP_W 24
-#define MAP_H 18
-#define CELL 50
+#define MAP_W 12
+#define MAP_H 9
+#define CELL 100
 
 typedef struct {
   double x, y;
@@ -16,7 +17,16 @@ typedef struct {
   Vec2 vel;
   double rad;
 } Camera;
-
+typedef struct {
+  int a;
+  int b;
+} idx;
+typedef struct {
+  idx a;
+  idx b;
+  idx c;
+  idx d;
+} vert;
 typedef struct {
   Vec2 v_hit;
   Vec2 h_hit;
@@ -32,19 +42,29 @@ Camera camera_default(void) {
 }
 
 void draw_map(SDL_Renderer *renderer, int *debug) {
+  idx walls[MAP_H * MAP_W];
+  vert wall_coords[sizeof(walls) / sizeof(walls[0])];
   int map[MAP_H][MAP_W] = {
-      {0, 1, 0, 1},
-      {1, 0, 1, 0},
-      {0, 1, 0, 1},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
   };
   int px = 0, py = 0;
-
   SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+  int i = 0;
   for (int w = 0; w < MAP_W; w++) {
     for (int h = 0; h < MAP_H; h++) {
       px = CELL * w;
       py = CELL * h;
-      if (map[h][w] == 1 && *debug == 0) {
+      if (map[h][w] == 1 && *debug == 1) {
+        walls[i] = (idx){h, w};
+        wall_coords[i] = (vert){{walls[i].a * CELL, walls[i].b * CELL},
+                                {(walls[i].a) * CELL, (walls[i].b + 1) * CELL},
+                                {(walls[i].a + 1) * CELL, walls[i].b * CELL},
+                                {(walls[i].a + 1) * CELL, (walls[i].b + 1) * CELL}};
+        i++;
         SDL_Rect r = {px, py, CELL, CELL};
         SDL_RenderFillRect(renderer, &r);
       } else {
@@ -56,10 +76,27 @@ void draw_map(SDL_Renderer *renderer, int *debug) {
     SDL_Rect r = {px, 0, 1, 300000};
     SDL_RenderFillRect(renderer, &r);
   }
+  printf("%d\n", i);
+  int len = sizeof(wall_coords) / sizeof(wall_coords[0]);
+  for (int k = 0; k < len; k++) {
+    printf("A: %d,%d   B: %d,%d   C: %d,%d   D: %d, %d  \n", wall_coords->a.a,
+           wall_coords->a.b, wall_coords->b.a, wall_coords->b.b, wall_coords->c.a,
+           wall_coords->c.b, wall_coords->d.a, wall_coords->d.b);
+  }
+  for (int j = 0; j < i; j++) {
+    // printf("A: %d, B: %d,C: %d,D: %d\n", (walls[i].a * CELL),(walls[i].b *
+    // CELL),(walls[j].a + 1) * CELL, (walls[j].b + 1) * CELL );
+  }
 }
-void first_check(SDL_Renderer *renderer, Camera *cam, Ray_params *ray) {
 
+int is_wall(double x, double y) {
+  if (fmod(x, CELL) == 0 || fmod(y, CELL) == 0) {
+    return 1;
+  }
+  return 0;
+}
 
+void first_check(SDL_Renderer *renderer, Camera *cam, Ray_params *ray, int *draw) {
   double dx, dy;
 
   // vertical
@@ -79,17 +116,36 @@ void first_check(SDL_Renderer *renderer, Camera *cam, Ray_params *ray) {
   ray->v_hit = (Vec2){cam->pos.x + dx, cam->pos.y + dx * (ray->dir.y / ray->dir.x)};
   ray->h_hit = (Vec2){cam->pos.x + dy * (ray->dir.x / ray->dir.y), cam->pos.y + dy};
 
+  // DEBUG: draw red box if
+  //
+  //
+  //
+  // IF  index of map[h][w] + 1 * CELL == v_hit.x or v_hit.y it's a wall, then seeking ==
+  // 0
+  //  if (1 == is_wall(ray->v_hit.x, ray->v_hit.x)){
+  //  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+  //
+  //  SDL_Rect h = {ray->h_hit.x, ray->h_hit.y, 8, 8};
+  //  SDL_Rect v = {ray->v_hit.x, ray->v_hit.y, 8, 8};
+  //  }
+  //  if (1 == is_wall(ray->v_hit.x, ray->v_hit.x)){
+  //  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+  //
+  //  SDL_Rect h = {ray->h_hit.x, ray->h_hit.y, 8, 8};
+  //  SDL_Rect v = {ray->v_hit.x, ray->v_hit.y, 8, 8};
+  //  }
+
   SDL_Rect h = {ray->h_hit.x, ray->h_hit.y, 8, 8};
   SDL_Rect v = {ray->v_hit.x, ray->v_hit.y, 8, 8};
 
-  SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
-  SDL_RenderFillRect(renderer, &h);
-  SDL_RenderFillRect(renderer, &v);
-
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 200);
+  if (*draw == 1) {
+    SDL_RenderFillRect(renderer, &h);
+    SDL_RenderFillRect(renderer, &v);
+  }
   SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 }
-void next_checks(SDL_Renderer *renderer, Ray_params *ray) {
-
+void next_checks(SDL_Renderer *renderer, Ray_params *ray, int *draw) {
   double dx, dy;
   if (ray->dir.x < 0) {
     dx = -CELL;
@@ -112,44 +168,47 @@ void next_checks(SDL_Renderer *renderer, Ray_params *ray) {
   SDL_Rect nh = {ray->h_hit.x, ray->h_hit.y, 8, 8};
   SDL_Rect nv = {ray->v_hit.x, ray->v_hit.y, 8, 8};
 
-  SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-  SDL_RenderFillRect(renderer, &nh);
+  if (*draw == 1) {
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 200);
+    SDL_RenderFillRect(renderer, &nh);
 
-  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-  SDL_RenderFillRect(renderer, &nv);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 200);
+    SDL_RenderFillRect(renderer, &nv);
+  }
 }
-void cast_ray(SDL_Renderer *renderer, Camera *cam, double deg) {
+void cast_ray(SDL_Renderer *renderer, Camera *cam, double deg, int *draw) {
   Ray_params ray;
   ray.dir = (Vec2){cos(deg), sin(deg)};
-  first_check(renderer, cam, &ray);
+  first_check(renderer, cam, &ray, draw);
 
   ray.v_hit = (Vec2){ray.v_hit.x, ray.v_hit.y};
   ray.h_hit = (Vec2){ray.h_hit.x, ray.h_hit.y};
 
-  for (int i = 0; i < (MAP_W + MAP_W/12); i++){
-  next_checks(renderer, &ray);
+  for (int i = 0; i < (MAP_W + MAP_W / 12); i++) {
+    next_checks(renderer, &ray, draw);
   }
 }
-void cast_rays(SDL_Renderer *renderer, Camera *cam){
+void cast_rays(SDL_Renderer *renderer, Camera *cam) {
   double deg = fmod(cam->rad, 2 * M_PI);
 
   if (deg < 0) deg += 2 * M_PI;
-  // printf("deg: %.3f PI\n", deg/M_PI);
 
-  double step = 2.0/FOV;
+  double step = 2.0 / FOV;
   double raydeg = deg - step * (FOV / 2.0);
-  for (int i = 0; i < FOV; i++){
-  if (i == 0){
-    printf("raydeg @0: %.3f\n", raydeg);
-  }
-  if (i == (FOV - 1)){
-    printf("raydeg @ max :  %.3f\n", raydeg);
-  }
-  cast_ray(renderer, cam, raydeg);
+  int draw;
+  for (int i = 0; i < FOV; i++) {
+    if (i == 0) {
+      draw = 1;
+    } else if (i == (FOV - 1)) {
+      draw = 1;
+      // printf("raydeg @ max :  %.3f\n", raydeg);
+    } else {
+      // draw = 0;
+      draw = 1;
+    }
+    cast_ray(renderer, cam, raydeg, &draw);
     raydeg += step;
   }
-
-
 }
 
 void update_player(Camera *cam) {
@@ -161,7 +220,7 @@ void update_player(Camera *cam) {
 }
 void draw_player(SDL_Renderer *renderer, Camera *cam) {
   SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
-  double line_length = 880;
+  double line_length = 150;
 
   SDL_Rect p = {cam->pos.x - CELL / 8.0, cam->pos.y - CELL / 8.0, CELL / 4, CELL / 4};
 
@@ -172,14 +231,14 @@ void draw_player(SDL_Renderer *renderer, Camera *cam) {
   SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
 
   // sin
-  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-  SDL_RenderDrawLine(renderer, cam->pos.x, cam->pos.y, cam->pos.x,
-                     cam->pos.y + cam->dir.y * line_length);
+  // SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+  // SDL_RenderDrawLine(renderer, cam->pos.x, cam->pos.y, cam->pos.x,
+  // cam->pos.y + cam->dir.y * line_length);
   // cos
 
-  SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-  SDL_RenderDrawLine(renderer, cam->pos.x, cam->pos.y,
-                     cam->pos.x + cam->dir.x * line_length, cam->pos.y);
+  // SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+  // SDL_RenderDrawLine(renderer, cam->pos.x, cam->pos.y,
+  //                    cam->pos.x + cam->dir.x * line_length, cam->pos.y);
 
   // dir vector
   SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
@@ -255,10 +314,6 @@ int main(int argc, char *argv[]) {
   }
 
   // TODO:
-  // make the ray check all intersections, not just the first possible one
-  // next_check.x += CELL, next_check.y += CELL * dir.y/dir.x, and the inverse for the
-  // other line direction. -ish? cast all of the rays - probably call this function in a
-  // loop?? maybe bring the loop into this function, and extract the calculations into a
   // helper draw pillars of some const / distance height - distance is easy math probably
   // also vary color based on distance? need to draw a map for this, array makes it easy,
   // then any coordinate%CELL == 0 is a hit? ish?
